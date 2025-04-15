80b550c84c9c67cf25de5c090bb0ad7c37455190
Peyton Grattino <pgrattino@skypeaktech.com>
2025-04-15 12:29:11 -0600

    fix: error with a special character

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-          <p style={{justifySelf:"center", alignSelf:"center"}}>Peyton Grattino's Portfolio</p>
+          <p style={{justifySelf:"center", alignSelf:"center"}}>Peyton Grattino&#39;s Portfolio</p>
           <p style={{justifySelf:"center", alignSelf:"center"}}>Currently in development. Check back soon for updates!</p>
       </main>
     </div>
